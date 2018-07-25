07720be21324c6ab6773be1c7ea5e24aa4de25a9
kharris91 <kfharris91@gmail.com>
2018-07-25 10:12:52 -0400

    Plotting nmds

--- NMDS_Script.R
+++ NMDS_Script.R
@@ -10,3 +10,25 @@ cpl5 <- cpl5[,-c(1:2)]
 cpl5.ord <- metaMDS(cpl5, trymax = 1000)
 # Summary of Ordination
 cpl5.ord
+## Plot cpl5.ord
+plot(cpl5.ord, type = "n")
+points(cpl5.ord, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
+text(cpl5.ord, display = "spec", cex=0.7, col="blue")
+## View as stress plot
+stressplot(cpl5.ord)
+## By adding a third dimension to the ordination, we will be able to create a 3d plot
+## This also lowers the stress values
+cpl5.ord.3d <- metaMDS(cpl5, trymax = 1000, k = 3)
+# View summary
+cpl5.ord.3d
+# Plot 2d again
+plot(cpl5.ord.3d, type = "n")
+points(cpl5.ord.3d, display = "sites", cex = 0.8, pch=21, col="red", bg="yellow")
+text(cpl5.ord.3d, display = "spec", cex=0.7, col="blue")
+# Plot in 3d
+library("vegan3d")
+library("rgl")
+ordirgl(cpl5.ord.3d, scaling = 3, display = "sites")
+orglpoints(cpl5.ord.3d, display = "sites", choices = 1:3, col = "red")
+orgltext(cpl5.ord.3d, display = "species", choices = 1:3, adj = 0.5,
+         col = "black")
